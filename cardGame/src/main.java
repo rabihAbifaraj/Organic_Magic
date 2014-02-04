f3a1afa4194e5e081c100641d64c772b36360de3
Rabih Abifaraj <rabih.abifaraj@gmail.com>
2014-02-03 18:13:48 -0600

    OPEN - task 1: Card Game Proof of Concept https://github.com/mbartling/Organic_Magic/issues/issues/issue/1

--- cardGame/src/main.java
+++ cardGame/src/main.java
@@ -1,29 +1,53 @@
 import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class main 
+public class main implements ActionListener, ListSelectionListener
 {
-
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run() {
-				GUIgenerate();
-			}
-		});
 
-	}
 	public JPanel createContentPane()
 	{
-		JPanel aWildPanel = new JPanel();
-		//all widgets will be placed here
 		
+		JPanel aWildPanel = new JPanel();//aWildPanel is the totalGui panel
+		JList elements;
+		
+		//all widgets will be placed here
+		String names[] = {"selectable","elements","a","b","c","d","e","f","g","h","i","j","k"};
+		//here is the scroll with selectable elements
+		elements = new JList(names);
+		elements.setVisibleRowCount(10);
+		elements.setFixedCellHeight(20);
+		elements.setFixedCellWidth(100);
+		elements.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		elements.addListSelectionListener(this);
+		
+		//space out the widgets in the gui using boxLayout
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+		
+		bottomPanel.add(Box.createRigidArea(new Dimension(10,0)));
+		bottomPanel.add(elements);
+		
+		aWildPanel.add(bottomPanel);
 		aWildPanel.setOpaque(true);
 		return aWildPanel;
+	}
+	
+	public void valueChanged(ListSelectionEvent e)
+	{
+		if(e.getValueIsAdjusting()==false)
+		{
+			
+		}
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		
 	}
 	
 	private static void GUIgenerate()
@@ -41,5 +65,17 @@ public class main
 		//make visible
 		frame.setVisible(true);
 		//frame.setResizable(true);
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run() {
+				GUIgenerate();
+			}
+		});
+
 	}
 }
